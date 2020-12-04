46cbb7c1ef7fd17d7da42d17d3cb896a289ff5d2
Sam Sheets <sam.sheets94@gmail.com>
2020-12-04 09:45:02 -0700

    SummaryTotal component

--- src/TotalsSection/TotalsSection.js
+++ src/TotalsSection/TotalsSection.js
@@ -1,27 +1,17 @@
 import React, {Component} from 'react';
-import {USCurrencyFormat} from '../App';
 import SummaryOption from '../SummaryOption/SummaryOption';
+import SummaryTotal from '../SummaryTotal/SummaryTotal';
 
 class TotalsSection extends Component {
     render () {
-
-
-            const total = Object.keys(this.props.selected).reduce(
-                (acc, curr) => acc + this.props.selected[curr].cost, 0);
-            
-            return (
-                <div>
-                    <SummaryOption 
-                        selected={this.props.selected}/>
-                    <div className="summary__total">
-                        <div className="summary__total__label">Total</div>
-                        <div className="summary__total__value">
-                        {USCurrencyFormat.format(total)}
-                        </div>
-                    </div>
-                </div>
-            );
-        
+        return (
+            <div>
+                <SummaryOption 
+                    selected={this.props.selected}/>
+                <SummaryTotal 
+                    selected={this.props.selected}/>
+            </div>
+        );
     };
 }
 
